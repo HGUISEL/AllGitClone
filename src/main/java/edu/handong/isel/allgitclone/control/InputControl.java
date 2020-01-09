ae7d82439f6d0a7022d8e284fad88206d3df8c5c
newForinux <21600065@handong.edu>
2020-01-09 17:26:30 +0900

    Change the standard query to "created_at" date.

---
+++ src/main/java/edu/handong/isel/allgitclone/control/InputControl.java
@@ -0,0 +1,69 @@
+package edu.handong.isel.allgitclone.control;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class InputControl {
+	private String lang;
+	private String fork_count;
+	private String created_date;
+	
+	public HashMap<String, String> getDefaultOption() {
+		HashMap<String, String> options = new HashMap<>();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Language : ");
+		lang = sc.nextLine();
+		
+		System.out.println("Range of forks : ");
+		fork_count = sc.nextLine();
+		
+		System.out.println("Range of created date (yyyy-mm-dd) : ");
+		created_date = sc.nextLine();
+		
+		String opt = "language:" + lang +
+					" forks:" + fork_count +
+					" created:" + created_date;
+		
+		options.put("q", opt);
+		options.put("sort", "forks");
+		options.put("page", String.valueOf(1));
+		options.put("per_page", String.valueOf(100));
+		
+		sc.close();
+		
+		return options;
+	}
+	
+	public void change_date(HashMap<String, String> options) throws ParseException {
+		String chg = options.get("q");
+		int index = chg.indexOf('-');
+		int month = Integer.parseInt(chg.substring(index + 1, index + 3));
+		int year = Integer.parseInt(chg.substring(index - 4, index));
+		int date = Integer.parseInt(chg.substring(index + 4, index + 6));
+		
+		String origin_date = chg.substring(index - 4, index + 6);
+		
+		if (month != 12)
+			month++;
+		
+		else {
+			year++;
+			month = 1;
+		}
+		
+		SimpleDateFormat dat = new SimpleDateFormat("yyyy-MM-dd");
+		Date convertedDate = dat.parse(year + "-" + month + "-" + date);
+		String changed_date = dat.format(convertedDate);
+		
+		chg = chg.replace(origin_date, changed_date);
+		
+		
+		options.replace("q", chg);
+		System.out.println(chg);
+	}
+	
+}
--- src/main/java/edu/handong/isel/allgitclone/control/RetroMain.java
+++ src/main/java/edu/handong/isel/allgitclone/control/RetroMain.java
@@ -5,32 +5,26 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.ParseException;
 import java.util.HashMap;
 
 public class RetroMain {
+	private static HashMap<String, String> options;
 	
-	
-	public static void main (String[] args) throws IOException, InterruptedException {
+	public static void main (String[] args) throws IOException, InterruptedException, ParseException {
 		
+		MainActivity act = new MainActivity();
+		InputControl set_input = new InputControl();
+		options = set_input.getDefaultOption();
 		int pages = 1;
-		String lit = "1000";
-		HashMap<String, String> options = new HashMap<>();
-		
-		//Example parameters
-		options.put("q", "language:java forks:200.." + lit);
-		options.put("page", String.valueOf(pages));
-		options.put("sort", "forks");	//default : descending sorting
-		options.put("per_page", String.valueOf(100));	//essential
 		
 		BufferedWriter bw = new BufferedWriter(new FileWriter(new File("gitService.txt"), true));
 		PrintWriter pw = new PrintWriter(bw, true);
 		
+		
+
 		// 1000 counts reference available
-		while(true) {
-			MainActivity act = new MainActivity();
-			
-			if (act.isCheck_blank())
-				break;
+		while(!act.isCheck_blank()) {
 			
 			while (pages != 11) {
 				
@@ -44,19 +38,19 @@ public class RetroMain {
 				System.out.println("current page : " + pages);
 				System.out.println(options.get("q"));
 
-				
 				if (!act.isCheck_over_limits())
 					pages++;
 				
 			}
 			
-			lit = String.valueOf(act.getLast_forks());
-			options.replace("q", "language:java forks:200.." + lit);
+			set_input.change_date(options);
 			pages = 1;
-
 		}
 		
 		pw.close();
 		return;
+		
+
 	}
+	
 }
