7f4dd0b8939e14dde2282b3016fed12fbd6a1409
newForinux <you_maybe@naver.com>
2020-08-04 16:14:02 +0900

    Change the method of working commit as Github API policy changed.

--- src/main/java/edu/handong/isel/allgitclone/control/GithubService.java
+++ src/main/java/edu/handong/isel/allgitclone/control/GithubService.java
@@ -7,13 +7,14 @@ import com.google.gson.JsonObject;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
 
 public interface GithubService {
 	
 	@Headers("Accept: application/vnd.github.cloak-preview")
-	@GET("repos/{owner}/{repo}/commits")
-	Call<JsonObject> getUserCommits(@QueryMap Map<String, String> lang);
+	@GET("repos/{owner_repo}/commits")
+	Call<JsonObject> getUserCommits(@Path("owner_repo") String owner_repo);
 	
 	@Headers("Accept: application/vnd.github.mercy-preview+json")
 	@GET("search/repositories")
